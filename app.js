f4c9dc89d9463dd69da450fc0532b41c23d31b15
Mahmoud-Ramzy <anatedo4545@gmail.com>
2022-09-21 00:24:41 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,47 @@
+let times=0;
+addEventListener("mousemove",function(event){
+        if(times%6===0){
+            let body= document.body;
+            let bubble = document.createElement("span")
+            let x=event.clientX;
+            let y=event.clientY;
+            bubble.style.left= `${x}px`;
+            bubble.style.top= `${y}px`;
+            let size= (Math.random() * 70 )+20;
+            bubble.style.width=`${size}px`
+            bubble.style.height=`${size}px`
+            console.log(event);
+            body.appendChild(bubble);
+            
+            setTimeout(()=>{
+                bubble.remove();
+            },3000 );
+        }
+        times++;
+});
+
+
+function bubbleGenerator(){
+    
+}
+
+
+function showNavbar(){
+    document.querySelector(".side-navbar").style.display="flex";
+    setTimeout(()=>{
+        document.querySelector(".side-navbar").style.height="100vh";
+        document.querySelector(".side-navbar").style.opacity="100";
+        document.querySelector(".menu-closed").style.height="100vh";
+    },100)
+
+}
+function hideNavbar(){
+    document.querySelector(".side-navbar").style.height="0";
+    document.querySelector(".side-navbar").style.opacity="0";
+    document.querySelector(".menu-closed").style.height="100px";
+    setTimeout(()=>{
+        document.querySelector(".side-navbar").style.display="none";
+    },500)
+    
+    
+}
